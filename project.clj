d9e636bbb9753df41103b396ff27c63eafe57cf2
Oswaldo Bueno <bueno@carpintaria.com>
2024-12-30 18:15:38 -0300

    bump versions

--- project.clj
+++ project.clj
@@ -1,17 +1,17 @@
-(defproject kelveden/clj-wiremock "1.9.0-SNAPSHOT"
+(defproject kelveden/clj-wiremock "1.10.0-SNAPSHOT"
   :description "Clojure bindings for WireMock"
-  :url "https://github.com/kelveden/clj-wiremock"
+  :url "https://github.com/zougue-mpms/clj-wiremock"
   :license {:name "Eclipse Public License - v 1.0"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[cheshire "5.10.2"]
-                 [clj-http "3.12.3"]
+  :dependencies [[cheshire "5.13.0"]
+                 [clj-http "3.13.0"]
                  [com.github.tomakehurst/wiremock "2.27.2"]
                  [org.clojure/clojure "1.11.0"]
-                 [org.clojure/tools.logging "1.2.4"]
-                 [org.slf4j/slf4j-api "1.7.36"]]
+                 [org.clojure/tools.logging "1.3.0"]
+                 [org.slf4j/slf4j-api "2.0.16"]]
   :jvm-opts ["-Dclojure.spec.check-asserts=true"]
-  :profiles {:dev {:dependencies [[metosin/ring-http-response "0.9.3"]
-                                  [org.slf4j/slf4j-simple "1.7.36"]
+  :profiles {:dev {:dependencies [[metosin/ring-http-response "0.9.4"]
+                                  [org.slf4j/slf4j-simple "2.0.16"]
                                   [slingshot "0.12.2"]]
                    :source-paths ["dev"]
                    :repl-options {:init-ns user}}})
